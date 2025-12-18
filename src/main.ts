06cb87da07d63d720f5ae33204c0c905f8140293
Lounnaci <ahmedlounnaci@gmail.com>
2025-12-18 20:56:11 +0100

    13

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
 // Importer la configuration
 import config from './config';
